93db66fae0f4f49f7d828af7854b6461894354c4
Hector Guarneros <48600637+hw17@users.noreply.github.com>
2025-01-02 14:53:43 +0100

    Update writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -10,6 +10,7 @@
 
 int main(int argc, char * argv[])
 {
+    //printf( "--Run write.c file--\n");
     openlog(NULL, 0, LOG_USER);
     if(argc != 3)
     {
@@ -22,17 +23,25 @@ int main(int argc, char * argv[])
     	char* write_str = argv[2];
     	char* file_str = argv[1];
     	size_t size = strlen(write_str);
-    	fd = open (file_str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    	// 0644 mode == S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IROTH
-    	if(fd == -1)
-    	{
+        //Open file for writing
+    	fd = open (file_str, O_WRONLY | O_CREAT | O_TRUNC, 0644); // 0644 mode == S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IROTH
+    	if(fd == -1){
         	syslog(LOG_ERR, "%s\n", strerror(errno));
         	return 1;
     	}
     	else {
+            //Write the string to the file
+            ssize_t bytesWriting = write(fd, write_str, size);
+            if(bytesWriting == -1){
+                syslog(LOG_ERR, "Error writing to file %s ", file_str);
+                close(fd);
+                return 1;
+            }
+            // Log the succesful write to syslog
         	syslog(LOG_DEBUG, "Writing %s to %s\n", write_str, file_str);
-        	write(fd, write_str, size);
+        	//write(fd, write_str, size);
         	close(fd);
+            closelog(); // need to consider this line
         	return 0;
     	}
     }
