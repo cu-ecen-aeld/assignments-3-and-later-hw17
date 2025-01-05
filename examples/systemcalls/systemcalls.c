1227344efba3018777687b3c28923f71c4d07abe
hector warneros <hectorwarneros@gmail.com>
2025-01-05 23:44:19 +0100

    05/01/2025: Submitted changes on finder-test.sh and systemcalls.c. Adding testfile.txt

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,17 @@
+/*
+ * Author: Hector Guarneros
+*/
+
 #include "systemcalls.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 /**
  * @param cmd the command to execute with system()
@@ -7,28 +20,36 @@
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
 */
+
 bool do_system(const char *cmd)
 {
-
 /*
- * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    //executing cmd
+    int ret = system(cmd);
 
+    //Check if the cmd's return value is 0 for success.
+    if (ret == -1 || WEXITSTATUS(ret) != 0){
+        return false;
+    }
     return true;
 }
 
+
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
+* @param count -The numbers of variables passed to the function. The variables are command
+*   to execute.
 *   followed by arguments to pass to the command
 *   Since exec() does not perform path expansion, the command to execute needs
 *   to be an absolute path.
 * @param ... - A list of 1 or more arguments after the @param count argument.
 *   The first is always the full path to the command to execute with execv()
 *   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
+* @return true if the command @param ... with arguments @param arguments were executed
+*   successfully
 *   using the execv() call, false if an error occurred, either in invocation of the
 *   fork, waitpid, or execv() command, or if a non-zero return value was returned
 *   by the command issued in @param arguments with the specified arguments.
@@ -45,12 +66,8 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
 /*
- * TODO:
  *   Execute a system command by calling fork, execv(),
  *   and wait instead of system (see LSP page 161).
  *   Use the command[0] as the full path to the command to execute
@@ -58,9 +75,36 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    pid_t pid = fork(); // Create a new process
+    int status;
+
+    if (pid == -1){
+        perror("fork failed");
+        va_end(args);
+        return false;
+    }
+    else if (pid == 0){
+        // Child process
+        if (execv(command[0], command) == -1){
+            perror("execv failed");
+            _exit(errno); // _exit to avoid flushing buffer
+        }
+    }
+    else{
+        // Parent process
+        if (waitpid(pid, &status, 0) == -1){
+            perror("waitpid");
+            va_end(args);
+            return false;
+        }
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0){
+            va_end(args);
+            return false;
+        }
+    }
 
     va_end(args);
-
     return true;
 }
 
@@ -80,20 +124,55 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
 
 /*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
+ *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a
+ *   refernce, redirect standard out to a file specified by outputfile.
  *   The rest of the behaviour is same as do_exec()
- *
 */
 
-    va_end(args);
+    pid_t pid = fork();
+    int status;
 
+    if (pid == -1){
+        perror("fork failed");
+        va_end(args);
+        return false;
+    }
+    else if (pid == 0){
+        int output_fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+        if (output_fd == -1){
+            perror("open failed");
+            _exit(errno);
+        }
+
+        if (dup2(output_fd, STDOUT_FILENO) == -1){
+            perror("dup2 failed");
+            _exit(errno);
+        }
+        close(output_fd);
+
+        if (execv(command[0], command) == -1){
+            perror("execv failed");
+            _exit(errno);
+        }
+    }
+    else{
+
+        if (waitpid(pid, &status, 0) == -1){
+            perror("waitpid");
+            va_end(args);
+            return false;
+        }
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0){
+            va_end(args);
+            return false;
+        }
+    }
+
+    va_end(args);
     return true;
 }
+
--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -18,7 +18,7 @@ then
 		echo "Using default value ${NUMFILES} for number of files to write"
 	else
 		NUMFILES=$1
-	fi	
+	fi
 else
 	NUMFILES=$1
 	WRITESTR=$2
@@ -48,9 +48,9 @@ then
 		exit 1
 	fi
 fi
-echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+#echo "Removing the old writer utility and compiling as a native application"
+#make clean
+#make
 
 for i in $( seq 1 $NUMFILES)
 do
